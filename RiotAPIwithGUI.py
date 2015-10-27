d4dad98f98177d9631b8cbba4279b95dae3ba95e
Anthony Kuntz <akuntz@cmu.edu>
2015-10-27 01:41:33 -0400

    Added more text in GUI

--- RiotAPIwithGUI.py
+++ RiotAPIwithGUI.py
@@ -43,8 +43,9 @@ class GUI(object):
 		self.canvas.create_image(0,0,image=self.bg,anchor=NW)
 		self.margin = self.width / 15
 		self.canvas.create_text(10,0,anchor=NW, text = "Enter stats and then press enter or click OK.", font = "Helvetica 10 bold",fill = "white")
-		self.canvas.create_text(10,15,anchor=NW, text = "CDR up to 100 is valid, since the results are interesting.", font = "Helvetica 10 bold",fill = "white")
-		self.canvas.create_text(10,30,anchor=NW, text = "Press r to restart.", font = "Helvetica 10 bold",fill = "white")
+		self.canvas.create_text(10,15,anchor=NW, text = "The calculator will return the top 5 most efficient spells for those stats.", font = "Helvetica 10 bold",fill = "white")
+		self.canvas.create_text(10,30,anchor=NW, text = "CDR up to 100 is valid, since the results are interesting.", font = "Helvetica 10 bold",fill = "white")
+		self.canvas.create_text(10,45,anchor=NW, text = "Press r to restart.", font = "Helvetica 10 bold",fill = "white")
 
 		self.canvas.create_text(5,self.height - 5, text = "Created by: Anthony Kuntz | Ninjasorcerer", anchor = W, font = "Helvetica 8", fill = "black")
 
